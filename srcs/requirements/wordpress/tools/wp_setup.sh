9cc75b7a54e489588cd179093df55ace1b07c80e
jens8n <jensen.bucoy@gmail.com>
2024-07-31 19:20:55 +0400

    mariadb won't connect

---
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+chmod 755 -R /var/www/html
+
+wp core download
+
+sudo -u www-data wp config create --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$DB_PASS \
+    --dbhost=$DB_HOST \
+    --dbprefix=$DB_PRE \
+    --allow-root
+
+sudo -u www-data wp core install --url=$DOMAIN_NAME \
+    --title="$WP_TITLE" \
+    --admin_user=$WP_USER \
+    --admin_password=$WP_PASS \
+    --admin_email=$WP_EMAIL \
+    --allow-root
+
+sudo -u www-data wp user create "$WP_OTHER_USER" $WP_OTHER_EMAIL \
+    --user_pass=$WP_OTHER_PASS \
+    --role=author \
+    --allow-root
+
+php-fpm7.4 -FR
+
+
+
+
+
